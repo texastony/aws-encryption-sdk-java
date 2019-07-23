b3df03b75b2661e7449a2bcf2227f74c9a01836a
PSHAYNA <pshayna@amazon.com>
2019-07-23 11:39:14 -0700

    Adding check for if ciphertext is complete in the ParsedCiphertext constructor and throwing a BadCiphertextException if it is not.

--- src/main/java/com/amazonaws/encryptionsdk/ParsedCiphertext.java
+++ src/main/java/com/amazonaws/encryptionsdk/ParsedCiphertext.java
@@ -1,11 +1,11 @@
 /*
  * Copyright 2016 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except
  * in compliance with the License. A copy of the License is located at
- * 
+ *
  * http://aws.amazon.com/apache2.0
- * 
+ *
  * or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
@@ -15,6 +15,9 @@ package com.amazonaws.encryptionsdk;
 
 import com.amazonaws.encryptionsdk.internal.Utils;
 import com.amazonaws.encryptionsdk.model.CiphertextHeaders;
+import com.amazonaws.encryptionsdk.exception.AwsCryptoException;
+import com.amazonaws.encryptionsdk.exception.BadCiphertextException;
+import com.amazonaws.encryptionsdk.exception.ParseException;
 
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
@@ -33,9 +36,13 @@ public class ParsedCiphertext extends CiphertextHeaders {
      * {@code ciphertext} and that any changes made to the backing array will be reflected here as
      * well.
      */
-    public ParsedCiphertext(final byte[] ciphertext) {
+    public ParsedCiphertext(final byte[] ciphertext) throws ParseException {
         ciphertext_ = Utils.assertNonNull(ciphertext, "ciphertext");
-        offset_ = deserialize(ciphertext_, 0);
+        if (ciphertext.isComplete()) {
+          offset_ = deserialize(ciphertext_, 0);
+        } else {
+          throw new BadCiphertextException("Incomplete ciphertext.");
+        }
     }
 
     /**

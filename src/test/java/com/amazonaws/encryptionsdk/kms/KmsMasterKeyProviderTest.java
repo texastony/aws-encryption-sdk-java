629dbfdaa91b6023034f808f5e4865fd7048c963
lavaleri <49660121+lavaleri@users.noreply.github.com>
2021-12-10 09:41:29 -0800

    chore: Upgrade mockito to 4.0.0 (#438)

--- src/test/java/com/amazonaws/encryptionsdk/kms/KmsMasterKeyProviderTest.java
+++ src/test/java/com/amazonaws/encryptionsdk/kms/KmsMasterKeyProviderTest.java
@@ -18,8 +18,8 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
 import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 
 import com.amazonaws.AmazonServiceException;
@@ -326,7 +326,7 @@ public class KmsMasterKeyProviderTest {
             () -> mkp.decryptDataKey(ALGORITHM_SUITE, inputEDKs, ENCRYPTION_CONTEXT));
 
         ArgumentCaptor<DecryptRequest> decrypt = ArgumentCaptor.forClass(DecryptRequest.class);
-        verifyZeroInteractions(client);
+        verifyNoInteractions(client);
         return;
       }
 
